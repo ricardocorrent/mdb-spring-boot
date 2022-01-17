c5cbbb19fd3e4bc380361e96de88af2c4a96143b
Ricardo Corrent <ricardocorrent@hotmail.com>
2022-01-17 17:29:07 -0300

    first commit

---
+++ src/main/kotlin/com/ricardocorrent/mdbsb/MdbSbApplication.kt
@@ -0,0 +1,11 @@
+package com.ricardocorrent.mdbsb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MdbSbApplication
+
+fun main(args: Array<String>) {
+	runApplication<MdbSbApplication>(*args)
+}
---
+++ src/test/kotlin/com/ricardocorrent/mdbsb/MdbSbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ricardocorrent.mdbsb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MdbSbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
